1cc99f042ca0f5fe859ed31eff2c1752aa87e518
ydah <t.yudai92@gmail.com>
2026-02-20 19:04:49 +0900

    Raise a helpful ArgumentError when `sidekiq_retries_exhausted` is not defined
    ## Summary
    
    Previously, calling `within_sidekiq_retries_exhausted_block` without defining `sidekiq_retries_exhausted` first would raise an unhelpful `NoMethodError: undefined method 'call' for nil`, making it difficult for users to identify the root cause.
    
    This PR adds an early guard clause that raises an `ArgumentError` with a clear message pointing users to the missing setup step.
    
    ## Before
    
    ```
    NoMethodError: undefined method 'call' for nil
    ```
    
    ## After
    
    ```
    ArgumentError: Define sidekiq_retries_exhausted before calling within_sidekiq_retries_exhausted_block
    ```

--- lib/rspec/sidekiq/helpers/within_sidekiq_retries_exhausted_block.rb
+++ lib/rspec/sidekiq/helpers/within_sidekiq_retries_exhausted_block.rb
@@ -4,6 +4,10 @@ module Sidekiq
   module Worker
     module ClassMethods
       def within_sidekiq_retries_exhausted_block(user_msg = {}, exception = default_retries_exhausted_exception, &block)
+        unless sidekiq_retries_exhausted_block
+          raise ArgumentError, 'Define `sidekiq_retries_exhausted` before calling `within_sidekiq_retries_exhausted_block`'
+        end
+
         block.call
         sidekiq_retries_exhausted_block.call(default_retries_exhausted_message.merge(user_msg), exception)
       end
